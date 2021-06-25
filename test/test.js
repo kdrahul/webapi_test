f70faa8eb61035efd062e87e75ab9a377a33eceb
Rahul Kulkarni <kdrahul@github.com>
2021-06-25 22:10:43 +0530

    Added test cases

--- test/test.js
+++ test/test.js
@@ -1,8 +1,27 @@
-const request = require('supertest').VERB();
-const desc = require('mocha');
+const server = require('../app');
+const desc = require('mocha').describe;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.should();
+chai.use(chaiHttp);
 
 desc('Boundary Value Analysis', () => {
-    desc('Case 1', () => {
-        it()
+    dates = [06];
+    it('Case 1: 15/aa/1900', (done) => {
+        chai.request(server)
+            .get("/nextdate/15/aa/1900")
+            .end((err, response) => {
+                response.should.have.status(400);
+                done()
+            });
+    });
+    it('Case 5: 30/3/2000', (done) => {
+        chai.request(server)
+            .get("/nextdate/30/3/2000")
+            .end((err, response) => {
+                response.should.have.status(200);
+                done()
+            });
     })
 });
