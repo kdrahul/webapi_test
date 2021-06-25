a48e7225fdb339dc697f389d0f4765ac5d24308e
Rahul Kulkarni <kdrahul@github.com>
2021-06-25 22:53:59 +0530

    Added BVA test cases

--- test/test.js
+++ test/test.js
@@ -6,22 +6,51 @@ const chaiHttp = require('chai-http');
 chai.should();
 chai.use(chaiHttp);
 
+
 desc('Boundary Value Analysis', () => {
-    dates = [06];
-    it('Case 1: 15/aa/1900', (done) => {
-        chai.request(server)
-            .get("/nextdate/15/aa/1900")
-            .end((err, response) => {
-                response.should.have.status(400);
-                done()
+    desc('Valid Values', () => {
+        dates = [
+            "30/03/2000",
+            "02/03/2000",
+            "15/11/2000",
+            "15/2/2000",
+            "15/3/2011",
+            "15/03/1913",
+        ];
+
+        for (let i = 0; i < dates.length; i++) {
+            it(`${dates[i]}`, (done) => {
+                chai.request(server)
+                    .get(`/nextdate/${dates[i]}`)
+                    .end((err, response) => {
+                        response.should.have.status(200);
+                        done()
+                    });
             });
+
+        }
     });
-    it('Case 5: 30/3/2000', (done) => {
-        chai.request(server)
-            .get("/nextdate/30/3/2000")
-            .end((err, response) => {
-                response.should.have.status(200);
-                done()
+    desc('Invalid Values', () => {
+        dates = [
+            "15/aa/1900",
+            "dd/06/1901",
+            "15/06/196y",
+            "00/03/2000",
+            "15/13/2000",
+            "15/03/1811",
+            "15/03/2013",
+        ];
+
+        for (let i = 0; i < dates.length; i++) {
+            it(`${dates[i]}`, (done) => {
+                chai.request(server)
+                    .get(`/nextdate/${dates[i]}`)
+                    .end((err, response) => {
+                        response.should.have.status(400);
+                        done()
+                    });
             });
-    })
+
+        }
+    });
 });
